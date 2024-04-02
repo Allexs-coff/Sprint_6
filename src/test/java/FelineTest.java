9dea8fddaad836d56c26039a164d876267378ce9
Alexey Sergeev <tender.bumblebee@gmail.com>
2024-04-02 15:52:59 +0300

    edits for the sprint 6 project

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.Mockito;
@@ -26,8 +27,15 @@ public class FelineTest {
     }
 
     @Test
-    public void getKittensWhithParams(){
+    public void getKittensWithParams(){
         assertEquals(feline.getKittens(1),1);
+        Mockito.verify(feline,Mockito.times(1)).getKittens(1);
+    }
+
+    @Test
+    public void getKittensWithParamsStabTest(){
+        Mockito.when(feline.getKittens(1)).thenReturn(100500);
+        assertEquals(feline.getKittens(1),100500);
         Mockito.verify(feline,Mockito.times(1)).getKittens(1);
     }
 
@@ -36,4 +44,6 @@ public class FelineTest {
         assertEquals(feline.getKittens(),1);
         Mockito.verify(feline,Mockito.times(1)).getKittens();
     }
+
+
 }
--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,17 +1,18 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.Mockito;
 import java.util.List;
-
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest {
-    @Spy   private Feline feline= new Feline();
+@Mock   private Feline feline= new Feline();
 @Spy private Lion lion  = new Lion("Самец", feline);
-@Spy private Lion lioness= new Lion("Самка", feline);;
+@Spy private Lion lioness= new Lion("Самка", feline);
     private Lion wrongValue;
 
     public LionTest() throws Exception {}
@@ -19,13 +20,13 @@ public class LionTest {
     @Test
     public void lionTest()  {
         assertTrue(lion.doesHaveMane());
-        Mockito.verify(lion,Mockito.times(1)).doesHaveMane();
+        verify(lion,Mockito.times(1)).doesHaveMane();
     }
 
     @Test
     public void lionessTest() {
         assertFalse(lioness.doesHaveMane());
-        Mockito.verify(lioness,Mockito.times(1)).doesHaveMane();
+        verify(lioness,Mockito.times(1)).doesHaveMane();
     }
 
     @Test
@@ -37,14 +38,13 @@ public class LionTest {
     @Test
     public void getKittensTest()  {
         assertEquals(lion.getKittens(),1);
-        Mockito.verify(lion,Mockito.times(1)).getKittens();
+        verify(lion,Mockito.times(1)).getKittens();
 
     }
 
     @Test
     public void getFoodTest() throws Exception {
         assertEquals(lion.getFood(), List.of("Животные", "Птицы", "Рыба"));
-        Mockito.verify(lion,Mockito.times(1)).getFood();
+        verify(lion,Mockito.times(1)).getFood();
     }
-
 }
